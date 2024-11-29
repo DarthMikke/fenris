c91c27f3b4440da31f242368fd7ea6316dde05d8
Michal Jan Warecki <mwarecki1@gmail.com>
2024-11-29 21:58:14 +0100

    Sort observations into bins with different YYYY-MM component

--- api/main.go
+++ api/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"millim.no/fenris/frost"
 
@@ -55,12 +56,12 @@ func statsHandler(w http.ResponseWriter, r *http.Request) {
 	if (err != nil) {
 		panic(err)
 	}
+
 	upstreamResponse, cached, err := frostApi.Observations(
 		[]string{stationId},
 		fmt.Sprintf("%d-01-01/%d-01-01", fromYear, toYear + 1),
 		[]string{"air_temperature"},
 	)
-
 	if (err != nil) {
 		panic(err)
 	}
@@ -68,15 +69,30 @@ func statsHandler(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("X-Cache-Hit", "1")
 	}
 
-	/*
-	response := new (map[string]any)
-	response["id"] = (upstreamResponse.Data[0]).Id
+	var decodedData frost.ObservationResponse
+	decoder := json.NewDecoder(strings.NewReader(*upstreamResponse))
+	decoder.Decode(&decodedData)
+
+	// Sort into bins with different YYYY-MM component in
+	// the referenceTime field.
+	var bins [][]frost.ObservationsAtRefTime
+	for i, e := range decodedData.Data {
+		if (i == 0) {
+			bins = append(bins, *new ([]frost.ObservationsAtRefTime))
+			bins[len(bins) - 1] = append(bins[len(bins) - 1], e)
+			continue
+		}
+		thisDate := e.ReferenceTime
+		previousDate := decodedData.Data[i - 1].ReferenceTime
+		if (thisDate[:7] != previousDate[:7]) {
+			bins = append(bins, *new ([]frost.ObservationsAtRefTime))
+		}
+		bins[len(bins) - 1] = append(bins[len(bins) - 1], e)
+	}
 
 	encoder := json.NewEncoder(w)
-	encoder.Encode(upstreamResponse.Data[0])
-	*/
-
-	fmt.Fprintf(w, *upstreamResponse);
+	encoder.Encode(bins)
+	// fmt.Fprintf(w, *upstreamResponse);
 }
 
 var frostApi *frost.Api
