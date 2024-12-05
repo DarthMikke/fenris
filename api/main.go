16e3e93923f226247e0f12b17fc79cec910eadc0
Michal Jan Warecki <mwarecki1@gmail.com>
2024-12-05 07:47:54 +0100

    Return map[string][]float64 with avg(min(T P1D) P1M), avg(max(T P1D) P1M), avg(avg(T P1D) P1M)

--- api/main.go
+++ api/main.go
@@ -78,36 +78,97 @@ func statsHandler(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(strings.NewReader(*upstreamResponse))
 	decoder.Decode(&decodedData)
 
-	// Sort into bins with different YYYY-MM component in
-	// the referenceTime field.
-	var series []stats.Measurement[[]frost.Observation]
+	var series []stats.Measurement[float64]
 	for _, v := range decodedData.Data {
-		series = append(series, stats.Measurement[[]frost.Observation]{
-			Timestamp: v.ReferenceTime,
-			Data: v.Observations,
-		})
+		/**
+		 * Multiple observations can be done at the observation time. If this is
+		 * the case, average them and pass a notice to the log/console.
+		 */
+		switch len(v.Observations) {
+		case 1:
+			series = append(series, stats.Measurement[float64]{
+				Timestamp: v.ReferenceTime,
+				Data: float64(v.Observations[0].Value),
+			})
+		default:
+			fmt.Printf("Multiple observations at %s\n", v.ReferenceTime)
+			series = append(series, stats.Measurement[float64]{
+				Timestamp: v.ReferenceTime,
+				Data: stats.AverageWithAccessor(v.Observations, func(o frost.Observation) float64 {
+					return float64(o.Value)
+				}),
+			})
+		}
 	}
-	var binsByMonth [][]stats.Measurement[[]frost.Observation]
-	binsByMonth = stats.Periodise(series, "P1M")
-	var threed [][][]stats.Measurement[[]frost.Observation]
-	threed = stats.Wrap(binsByMonth, 12)
+
+	// Sort into bins with different YYYY-MM-DD component in
+	// the referenceTime field.
+	var binsByDay []stats.Measurement[DailySummary]
+	for _, v := range stats.Periodise(series, "P1D") {
+		min := v[0]
+		max := v[0]
+		for _, w := range v {
+			if w.Data < min.Data {
+				min = w
+			}
+			if w.Data > max.Data {
+				max = w
+			}
+		}
+		newSummary := stats.Measurement[DailySummary] {
+			Timestamp:	v[0].Timestamp,
+			Data:	DailySummary {
+				Min: min.Data,
+				Max: max.Data,
+				Avg: stats.AverageWithAccessor(v, func(o stats.Measurement[float64]) float64 {
+					return o.Data
+				}),
+			},
+		}
+		fmt.Println(v, newSummary)
+		binsByDay = append(binsByDay, newSummary)
+	}
+
+	encoder := json.NewEncoder(w)
+	binsByMonth := stats.Periodise(binsByDay, "P1M")
+	threed := stats.Wrap(binsByMonth, 12)
 	threed, err = stats.Transpose(threed)
 	if (err != nil) {
 		panic(err)
 	}
-	var flattened [][]stats.Measurement[[]frost.Observation]
-	flattened = stats.Flatten3D(threed)
+	flattened := stats.Flatten3D(threed)
 
 	averages := stats.Reduce(
 		flattened,
-		func(obs []stats.Measurement[[]frost.Observation]) float64 {
+		func(obs []stats.Measurement[DailySummary]) float64 {
 			var numbers []float64
 			for _, e := range obs {
-				numbers = append(numbers, float64(e.Data[0].Value))
+				numbers = append(numbers, float64(e.Data.Avg))
 			}
 			return stats.Average(numbers)
 		},
 	)
+	avgmaxs := stats.Reduce(
+		flattened,
+		func(obs []stats.Measurement[DailySummary]) float64 {
+			var numbers []float64
+			for _, e := range obs {
+				numbers = append(numbers, float64(e.Data.Max))
+			}
+			return stats.Average(numbers)
+		},
+	)
+	avgmins := stats.Reduce(
+		flattened,
+		func(obs []stats.Measurement[DailySummary]) float64 {
+			var numbers []float64
+			for _, e := range obs {
+				numbers = append(numbers, float64(e.Data.Min))
+			}
+			return stats.Average(numbers)
+		},
+	)
+	/*
 	maxs := stats.Reduce(
 		flattened,
 		func(obs []stats.Measurement[[]frost.Observation]) stats.Measurement[float64] {
@@ -136,12 +197,16 @@ func statsHandler(w http.ResponseWriter, r *http.Request) {
 			return stats.AnnotatedMin(numbers)
 		},
 	)
+	*/
 
-	encoder := json.NewEncoder(w)
 	encoder.Encode(map[string]any{
 		"average": averages,
+		"avgmax": avgmaxs,
+		"avgmin": avgmins,
+		/*
 		"max": maxs,
 		"min": mins,
+		*/
 	})
 	// fmt.Fprintf(w, *upstreamResponse);
 }
