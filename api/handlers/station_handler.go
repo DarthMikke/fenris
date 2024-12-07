0a51100abdc3837d04b657b387690fad8a4e3dfd
Michal Jan Warecki <mwarecki1@gmail.com>
2024-12-07 22:56:38 +0100

    Nicer dependency injection.

--- api/handlers/station_handler.go
+++ api/handlers/station_handler.go
@@ -6,31 +6,29 @@ import (
 	"millim.no/fenris/frost"
 )
 
-func StationHandler(f *frost.Api) http.HandlerFunc {
-	return func (w http.ResponseWriter, r *http.Request) {
-		stationId := r.PathValue("stationId")
+func StationHandler(f *frost.Api, w http.ResponseWriter, r *http.Request) {
+	stationId := r.PathValue("stationId")
 
-		/*
-		response := responses.StationResponse{
-			StationId: stationId,
-		}
-		*/
-
-		upstreamResponse, cached, err := f.Sources([]string{stationId})
-
-		if (err != nil) {
-			panic(err)
-		}
-		if (cached) {
-			w.Header().Add("X-Cache-Hit", "1")
-		}
-
-		/*
-		response := new (map[string]any)
-		response["id"] = (upstreamResponse.Data[0]).Id
-		*/
-
-		encoder := json.NewEncoder(w)
-		encoder.Encode(upstreamResponse.Data[0])
+	/*
+	response := responses.StationResponse{
+		StationId: stationId,
 	}
+	*/
+
+	upstreamResponse, cached, err := f.Sources([]string{stationId})
+
+	if (err != nil) {
+		panic(err)
+	}
+	if (cached) {
+		w.Header().Add("X-Cache-Hit", "1")
+	}
+
+	/*
+	response := new (map[string]any)
+	response["id"] = (upstreamResponse.Data[0]).Id
+	*/
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(upstreamResponse.Data[0])
 }
--- api/main.go
+++ api/main.go
@@ -17,14 +17,30 @@ func indexHandler(w http.ResponseWriter, _ *http.Request) {
 
 var frostApi *frost.Api
 
+type SrvWrapper struct {
+	frostApi 	*frost.Api
+}
+
+func (s SrvWrapper) HandleFunc(
+	pattern string,
+	handler func(*frost.Api, http.ResponseWriter, *http.Request),
+) {
+
+	http.HandleFunc(pattern, func (w http.ResponseWriter, r *http.Request) {
+		handler(s.frostApi, w, r)
+	})
+}
+
 func main() {
 	godotenv.Load()
 
 	frostApi = &frost.Api {}
 	frostApi.Setup(os.Getenv("client_id"),os.Getenv("client_secret"))
 
-	http.HandleFunc("/api/s/{stationId}/from/{fromYear}/to/{toYear}", handlers.StatsHandler(frostApi))
-	http.HandleFunc("/api/s/{stationId}", handlers.StationHandler(frostApi))
+	server := SrvWrapper{frostApi: frostApi}
+
+	server.HandleFunc("/api/s/{stationId}/from/{fromYear}/to/{toYear}", handlers.StatsHandler)
+	server.HandleFunc("/api/s/{stationId}", handlers.StationHandler)
 	// http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":5000", nil)
 }
